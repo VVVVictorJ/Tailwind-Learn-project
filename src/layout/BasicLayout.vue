e74dd9d3ad9c3b305b465a221bfb32bad9562f63
VVVVictorJ <VictorRobin1097@gmail.com>
2025-05-11 21:37:29 +0800

    1、修改左侧导航栏，添加两个界面 2、HomeView 改成grid布局 3、添加路由

---
+++ src/layout/BasicLayout.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-sider class="side" v-model:collapsed="collapsed" collapsible>
+        <div class="logo flex items-center justify-center bg-white/30 border-r border-gray-200" style="height: 64px">
+          <img src="../assets/logo.svg" alt="logo" class="h-1/2" />
+        </div>
+        <a-divider style="margin: 0" />
+
+        <a-menu theme="light" mode="inline" :default-selected-keys="[$route.path]">
+          <a-menu-item key="/">
+            <router-link to="/" class="flex items-center gap-10">
+              <WifiOutlined />
+              <span class="nav-text">首页</span>
+            </router-link>
+          </a-menu-item>
+          <a-menu-item key="/about">
+            <router-link to="/about" class="flex items-center gap-10">
+              <VideoCameraOutlined />
+              <span class="nav-text">关于</span>
+            </router-link>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header class="header">Header</a-layout-header>
+        <a-layout-content class="content">
+          <router-view />
+        </a-layout-content>
+        <a-layout-footer class="footer">2025@copyright by victor</a-layout-footer>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+// import HomeView from '@/views/HomeView.vue'
+import { WifiOutlined, VideoCameraOutlined } from '@ant-design/icons-vue'
+import { RouterLink, useRoute } from 'vue-router'
+import { Divider as ADivider } from 'ant-design-vue'
+
+const collapsed = ref<boolean>(true)
+const $route = useRoute()
+</script>
+
+<style scoped>
+#basicLayout .side {
+  background-color: white;
+  width: 100px;
+}
+
+#basicLayout .header {
+  padding-inline: 20px;
+  background: white;
+  color: unset;
+  margin-bottom: 16px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  position: fixed;
+  padding: 16px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+#basicLayout .content {
+  padding: 20px 20px 44px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  height: calc(100vh - 64px - 48px); /* 确保内容区域有明确的高度 */
+}
+
+.logo-divider {
+  height: 1px;
+  background: #e5e7eb; /* gray-200 */
+  width: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid grid-cols-3 gap-4 h-full">
+    <div class="h-full bg-gray-500">01</div>
+    <div class="h-full bg-gray-500">02</div>
+    <div class="h-full bg-gray-500">03</div>
+    <div class="col-span-2 h-full bg-gray-500">04</div>
+    <div class="h-full bg-gray-500">05</div>
+    <div class="h-full bg-gray-500">06</div>
+    <div class="col-span-2 h-full bg-gray-500">07</div>
+  </div>
 </template>
+
+<script setup lang="ts">
+
+
+
+</script>
